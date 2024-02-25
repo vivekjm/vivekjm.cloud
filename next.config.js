54e3df19408a91104a55c3489a960cb85031619b
Vivek JM <vivekjm77@gmail.com>
2024-02-26 02:39:12 +0530

    export

--- next.config.js
+++ next.config.js
@@ -1,40 +1,41 @@
 module.exports = {
-	reactStrictMode: true,
-	env: {
-		dir: '/',
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**.vercel.app',
-				pathname: '/api/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.vercel.app',
-				pathname: '?app=portfolio-theme-jqe0jhmif-atlamors.vercel.app'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.shields.io',
-				pathname: '/badge/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.shields.io',
-				pathname: '/github/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.githubusercontent.com',
-				pathname: '/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.medium.com',
-				pathname: '/**'
-			},
-		],
-	},
-}
+  reactStrictMode: true,
+  env: {
+    dir: "/",
+  },
+  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.vercel.app",
+        pathname: "/api/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.vercel.app",
+        pathname: "?app=portfolio-theme-jqe0jhmif-atlamors.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "**.shields.io",
+        pathname: "/badge/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.shields.io",
+        pathname: "/github/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.medium.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
