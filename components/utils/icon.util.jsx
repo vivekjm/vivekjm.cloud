80b45589a7983d2b507e7ccca37fe6300e505453
Vivek JM <vivekjm77@gmail.com>
2024-02-29 02:03:32 +0530

    Update icon.util.jsx

--- components/utils/icon.util.jsx
+++ components/utils/icon.util.jsx
@@ -4,11 +4,6 @@ import { useEffect, useState } from 'react'
 // Font Awesome packages
 const { library, config } = require('@fortawesome/fontawesome-svg-core')
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas } from '@fortawesome/pro-solid-svg-icons'
-import { fat } from '@fortawesome/pro-thin-svg-icons'
-import { fal } from '@fortawesome/pro-light-svg-icons'
-import { fad } from '@fortawesome/pro-duotone-svg-icons'
-import { far } from '@fortawesome/pro-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // Load icons into
@@ -37,7 +32,7 @@ export default function Icon({ icon }) {
 
 	const [ stateIconKey, setIconKey ] = useState('circle-notch')
 
-	useEffect( () => setIconKey( iconKey ), [ iconKey ] )
+	useEffect( () => setIconKey( 'fab' ), [ iconKey ] )
 
 	return (
 		<FontAwesomeIcon icon={[ iconType, stateIconKey ]} />
